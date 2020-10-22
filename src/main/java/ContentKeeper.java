9ebc47467c807907707fc5d7df43be3272b812a0
stanmb <andrewyankow@gmail.com>
2020-10-22 08:47:03 +0300

    Trying to read about from resources

--- src/main/java/ContentKeeper.java
+++ src/main/java/ContentKeeper.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 public class ContentKeeper {
     // Initialization path to the About file
     File file = new File("C:\\Users\\andre\\Desktop\\about.txt");
+
+
     //method gets about information from file and returns it in String variable
     public String getAbout() {
         String about = "";
         String nextLine;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/resources/about")));
             while ((nextLine = reader.readLine()) != null) {
                 about += nextLine;
                 about += "\n" + "\n" + "\n";
